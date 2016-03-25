941473a93b4210c2c83a5dad2213253d19e0e159
Caleb Chesnut <calebchesnut@me.com>
2016-03-25 16:28:01 -0500

    building

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,25 +3,43 @@ var React = require('react');
 module.exports = React.createClass({
   render: function () {
     return (
-    <div>
-      <div className="hero">
-        <h1>Hand Crafted Digital Products</h1>
+      <Header />
+    );
+  }
+});
+
+var Header = React.createClass({
+  render: function() {
+    return (
+      <div className="header">
+        <Menu />
+        <HeroSection />
       </div>
-      <h1>This is way coool</h1>
-      <HeroSection />
-    </div>
+    );
+  }
+});
+
+var Menu = React.createClass({
+  render: function() {
+    return (
+      <div className="menu">
+        <a className="menu__title">Menu</a>
+      </div>
     );
   }
 });
 
 var HeroSection = React.createClass({
-render: function() {
-  return (
-    <div>
-    We are Brand New Ship
-    </div>
-  );
-}
+  render: function() {
+    return (
+      <div className="hero">
+        <div className="hero__title">
+          <h1>Hand Crafted Digital Products</h1>
+        </div>
+        <h1>This is way coool</h1>
+      </div>
+    );
+  }
 });
 
 
