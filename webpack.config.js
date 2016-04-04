92db78355a9776ccc0236f898162a768842dd0ad
Caleb Chesnut <caleb.chesnut@integritystl.com>
2016-04-04 17:28:47 -0500

    adding image support for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -40,8 +40,19 @@ var config = {
       },
       {
         test: /\.scss$/,
-        loader: 'style!css!sass'
-       }
+        loaders: [
+          'style',
+          'css',
+          'autoprefixer?browsers=last 3 versions'
+        ]
+       },
+       {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'url?limit=8192',
+          'img'
+        ]
+      },
     ]
   },
   sassLoader: {
